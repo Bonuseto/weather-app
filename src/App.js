fb1bc6a99ae4137f4e0419ecd1cbf792fd201017
Yevgeniy Fomin <fomin.yevgeniy@gmail.com>
2023-09-12 13:35:31 +0200

    weather app poc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
 import './App.css';
 
 function App() {
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: '7b5f855dea5801df1df9469b143c0742',
+    lat: '43.23316706954475',
+    lon: '143.0836792826125',
+    lang: 'en',
+    unit: 'metric', // values are (metric, standard, imperial)
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactWeather
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        data={data}
+        lang="en"
+        unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+        showForecast
+      />
     </div>
   );
 }
